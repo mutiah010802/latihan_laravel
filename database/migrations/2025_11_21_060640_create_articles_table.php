42dd5d41f7934e0a6302ffc31a2ae92e067e98a6
mutiah010802 <putrimutiahnisa@gmail.com>
2025-11-21 14:04:36 +0700

    first init

---
+++ database/migrations/2025_11_21_060640_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignid('user_id'); 
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body'); 
+            $table->string('image')->nullable(); 
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade'); // Jika user dihapus, artikel ikut terhapus
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
